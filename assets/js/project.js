a7ec523a4e64131e00ef453ee967b54d791537ee
xLAVAxMASTERx <93382134+xLAVAxMASTERx@users.noreply.github.com>
2022-11-03 16:07:27 +0530

    update

--- assets/js/project.js
+++ assets/js/project.js
@@ -2,17 +2,16 @@ AOS.init();
 /* Project Cards */
 
 const projectcards = document.querySelector(".projectcards");
-/*
 // Array of object for projects
 const projects = [
   {
-    title: "Quiz App",
-    cardImage: "assets/images/project-page/quiz.jpg",
-    description: "A quiz app built using basic react.",
+    title: "RECON",
+    cardImage: "assets/images/project-page/RN07.jpeg",
+    description: "An Octa Raspberry Pi ARM Cluster",
     tagimg: "https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png",
-    Previewlink: "",
-    Githublink: "",
-  },
+    Previewlink: "http://recon.mu-enigma.org/",
+    Githublink: "https://github.com/MU-Enigma/RECON-setup",
+  },/*
   {
     title: "Chess Engine",
     cardImage: "assets/images/project-page/chess.jpg",
@@ -108,7 +107,7 @@ const projects = [
       "https://camo.githubusercontent.com/888e388801f947dec7c3d843942c277af25fe2b1aed1821542c4e711f210312a/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f7468756d622f632f63332f507974686f6e2d6c6f676f2d6e6f746578742e7376672f37363870782d507974686f6e2d6c6f676f2d6e6f746578742e7376672e706e67",
     Previewlink: "",
     Githublink: "",
-  },
+  },*/
 ];
 
 // function for rendering project cards data
@@ -161,4 +160,3 @@ function myFunction() {
     }
   }
 }
-*/
